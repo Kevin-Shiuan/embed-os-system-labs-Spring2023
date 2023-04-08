0655a001a601121b0e5f474091646231f750e8a1
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-04-08 17:38:56 +0800

    hw4 done

---
+++ HW4/README.md
@@ -0,0 +1,98 @@
+# HW2 Mbed BLE programming - Peripheral in mbed-os (stm32) and Central in Python (RPi)
+
+1.  ### open the project in mbed studio
+
+    file -> open workspace -> choose the root folder of this project, which is HW4
+
+    ./HW2/STM32CubeL4/Projects/B-L475E-IOT01A/Applications/WiFi/WiFi_Client_Server/SW4STM32/B-L475E-IOT01
+
+2.  ### modify project files
+
+    #### i. at ./HW2/STM32CubeL4/Projects/B-L475E-IOT01A/Applications/WiFi/WiFi_Client_Server/Inc/main.h, add the following lines
+
+    ```cpp
+    #include "stm32l475e_iot01_accelero.h"
+    #include "stm32l475e_iot01_gyro.h"
+    #include "stm32l475e_iot01_qspi.h"
+    ```
+
+**Note:** you should include the libraries of the sensors. We included the path of library in our project, you may need to add the path yourself, you can refer to this [document](https://wiki.st.com/stm32mcu/wiki/STM32StepByStep:Step4_Sensors_usage)
+
+#### ii. at ./HW2/STM32CubeL4/Projects/B-L475E-IOT01A/Applications/WiFi/WiFi_Client_Server/INC/stm32l4xx_hal_conf.h, uncomment line 62
+
+```cpp
+#define HAL_QSPI_MODULE_ENABLED
+```
+
+3.  ### edit main.c & main.h
+    ./B-L475E-IOT01A/Applications/WiFi/WiFi_Client_Server/Inc/main.h should have the following libraries
+    ```cpp
+    /* Includes ------------------------------------------------------------------*/
+    #include "wifi.h"
+    #include "stm32l475e_iot01.h"
+    #include "stm32l475e_iot01_accelero.h"
+    #include "stm32l475e_iot01_gyro.h"
+    #include "stm32l475e_iot01_qspi.h"
+    #include "stdio.h"
+    ```
+    In ./B-L475E-IOT01A/Applications/WiFi/WiFi_Client_Server/Src/main.c, configure the wifi network name & password, IP address & port of the TCP server
+
+    ```cpp
+    #include "stm32l475e_iot01_accelero.h"
+    #include "stm32l475e_iot01_gyro.h"
+    #include "stm32l475e_iot01_qspi.h"
+
+    // WiFi network name and password
+    #define SSID_NAME "<your wifi name>"
+    #define SSID_PSWD "<your wifi password>"
+
+    // IP address and port of the TCP server
+    uint8_t RemoteIP[] = {192,168, xxx, yyy};
+    #define RemotePORT <port>
+    ```
+
+4. ### build and run
+
+   Build the project and run it on the board
+
+5. ### set up the TCP server
+
+    Our TCP server is coded in python, you can find the code in TCPserver.py. Before running the server, open the file and modify the following lines
+
+    ```python
+    HOST = '<IP>'  # IP address
+    PORT = 65431  # Port to listen on (use ports > 1023)
+    DATA_SIZE = 50 # number of data to be displayed in the graph simultaneously, you can change it to any number you want
+    ```
+    
+    To run the server in desktop, you can use the following command in the terminal
+
+    ```bash
+    python3 TCPserver.py 
+    ```
+
+6. ### connection between the board and the TCP server
+
+   The board will try to connect to the TCP server. Status of the board will be shown on the serial monitor, which is **Command Shell Console** of STM32CubeIDE. the baud rate is _115200_.
+
+7. ### communication between the board and the TCP server
+
+   The board will send the data to the TCP server every time server is requesting for data, which is every 200ms in our code. The raw data is send in json format, which is easy to parse in python. The data is in the following format.
+
+   ```json
+   {
+       "acce": {
+           "x": 0,
+           "y": 0,
+           "z": 0,
+       },
+       "gyro": {
+           "x": 0.0,
+           "y": 0.0,
+           "z": 0.0,
+       }
+   }
+   ```
+
+### demo video
+https://youtu.be/hdbEdIsr6-M
---
+++ HW4/mbed-os-magneto/source/main.cpp
@@ -0,0 +1,185 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <events/mbed_events.h>
+#include "ble/BLE.h"
+#include "ble/gap/Gap.h"
+#include "ble/services/HeartRateService.h"
+#include "pretty_printer.h"
+#include "mbed-trace/mbed_trace.h"
+#include "stm32l475e_iot01_magneto.h"
+
+using namespace std::literals::chrono_literals;
+
+const static char DEVICE_NAME[] = "MagnetoSensor";
+
+static events::EventQueue event_queue(/* event count */ 16 * EVENTS_EVENT_SIZE);
+
+class MagnetoDemo : ble::Gap::EventHandler {
+public:
+    MagnetoDemo(BLE &ble, events::EventQueue &event_queue) :
+        _ble(ble),
+        _event_queue(event_queue),
+        _magneto_uuid(GattService::UUID_HEART_RATE_SERVICE),
+        _magneto_value(100),
+        _magento_service(ble, _magneto_value, HeartRateService::LOCATION_FINGER),
+        _adv_data_builder(_adv_buffer)
+    {
+    }
+
+    void start()
+    {
+        _ble.init(this, &MagnetoDemo::on_init_complete);
+
+        _event_queue.dispatch_forever();
+    }
+
+private:
+    /** Callback triggered when the ble initialization process has finished */
+    void on_init_complete(BLE::InitializationCompleteCallbackContext *params){
+        if (params->error != BLE_ERROR_NONE) {
+            printf("Ble initialization failed.");
+            return;
+        }
+
+        print_mac_address();
+
+        /* this allows us to receive events like onConnectionComplete() */
+        _ble.gap().setEventHandler(this);
+
+        /* heart rate value updated every second */
+        _event_queue.call_every(
+            1000ms,
+            [this] {
+                update_sensor_value();
+            }
+        );
+
+        /* init sensor */
+        BSP_MAGNETO_Init();
+
+        start_advertising();
+    }
+
+    void start_advertising(){
+        /* Create advertising parameters and payload */
+
+        ble::AdvertisingParameters adv_parameters(
+            ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
+            ble::adv_interval_t(ble::millisecond_t(100))
+        );
+
+        _adv_data_builder.setFlags();
+        _adv_data_builder.setAppearance(ble::adv_data_appearance_t::GENERIC_HEART_RATE_SENSOR);
+        _adv_data_builder.setLocalServiceList({&_magneto_uuid, 1});
+        _adv_data_builder.setName(DEVICE_NAME);
+
+        /* Setup advertising */
+
+        ble_error_t error = _ble.gap().setAdvertisingParameters(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            adv_parameters
+        );
+
+        if (error) {
+            printf("_ble.gap().setAdvertisingParameters() failed\r\n");
+            return;
+        }
+
+        error = _ble.gap().setAdvertisingPayload(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            _adv_data_builder.getAdvertisingData()
+        );
+
+        if (error) {
+            printf("_ble.gap().setAdvertisingPayload() failed\r\n");
+            return;
+        }
+
+        /* Start advertising */
+
+        error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            printf("_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+
+        printf("Magneto sensor advertising, please connect\r\n");
+    }
+
+    void update_sensor_value(){
+
+        BSP_MAGNETO_GetXYZ(this -> pDataXYZ);
+
+        _magento_service.updateHeartRate(pDataXYZ[0]);
+        _magento_service.updateHeartRate(pDataXYZ[1]);
+        _magento_service.updateHeartRate(pDataXYZ[2]);
+    }
+
+    /* these implement ble::Gap::EventHandler */
+private:
+    /* when we connect we stop advertising, restart advertising so others can connect */
+    virtual void onConnectionComplete(const ble::ConnectionCompleteEvent &event){
+        if (event.getStatus() == ble_error_t::BLE_ERROR_NONE) {
+            printf("Client connected, you may now subscribe to updates\r\n");
+        }
+    }
+
+    /* when we connect we stop advertising, restart advertising so others can connect */
+    virtual void onDisconnectionComplete(const ble::DisconnectionCompleteEvent &event){
+        printf("Client disconnected, restarting advertising\r\n");
+
+        ble_error_t error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            printf("_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+    }
+
+private:
+    BLE &_ble;
+    events::EventQueue &_event_queue;
+
+    UUID _magneto_uuid;
+    uint16_t _magneto_value;
+    int16_t pDataXYZ[3] = {0};
+    HeartRateService _magento_service;
+
+    uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
+    ble::AdvertisingDataBuilder _adv_data_builder;
+};
+
+/* Schedule processing of events from the BLE middleware in the event queue. */
+void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context)
+{
+    event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
+}
+
+int main()
+{
+    mbed_trace_init();
+
+    BLE &ble = BLE::Instance();
+    ble.onEventsToProcess(schedule_ble_events);
+
+    MagnetoDemo demo(ble, event_queue);
+    demo.start();
+
+    return 0;
+}
+
