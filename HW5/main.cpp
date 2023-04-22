26f3d56cdf1e9a9cd9f7c2196970719db2f04e06
b04502136 <100915845+b04502136@users.noreply.github.com>
2023-04-22 18:04:40 +0800

    Add files via upload

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -1,13 +1,6 @@
-/*
- * Copyright (c) 2014-2020 Arm Limited and affiliates.
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
-// Adjust pin name to your board specification.
-// You can use LED1/LED2/LED3/LED4 if any is connected to PWM capable pin,
-// or use any PWM capable pin, and see generated signal on logical analyzer.
+// use PWM capable pin, and see generated signal on logical analyzer.
 PwmOut led(PA_15);
 
 int main() {
@@ -27,13 +20,10 @@ int main() {
 		0.5
 	};
 	// specify period first
-	led.period(1.0f);      // 4 second period
-	//led.write(0.50f);      // 50% duty cycle, relative to period
+	led.period(1.0f);      // 1 second period
+
 	for(i = 0; 1; i = (i + 1) % 12) {
-		led.write(duty[i]);
+		led.write(duty[i]);	// set duty cycle
 		ThisThread::sleep_for(1s);
 	}
-	//led = 0.5f;          // shorthand for led.write()
-	//led.pulsewidth(2);   // alternative to led.write, set duty cycle time in seconds
-	while (1);
 }
